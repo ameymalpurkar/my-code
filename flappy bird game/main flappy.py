bccc0959713079d9253051e0783fe47fc4d625a8
ameymalpurkar <112706862+ameymalpurkar@users.noreply.github.com>
2023-08-12 15:40:28 +0530

    Add files via upload
    now it is working yee but sounds are not added in the game

--- flappy bird game/main flappy.py
+++ flappy bird game/main flappy.py
@@ -14,6 +14,9 @@ def main():
   bird_image = pygame.image.load("assets/flappy_bird.png")
   pipe_image = pygame.image.load("assets/pipe.png")
 
+  # Load the sounds
+
+
   # Create the bird
   bird = pygame.Rect(100, 200, 50, 50)
 
@@ -21,7 +24,9 @@ def main():
   pipes = []
   for i in range(0, 100000):
     pipe_height = random.randint(0, 300)
-    pipes.append(pygame.Rect(200 + i * 100, pipe_height, 100, 200))
+    upper_pipe_height =  - pipe_height
+    pipes.append(pygame.Rect(200 + i * 100, pipe_height, 50, 20))
+    pipes.append(pygame.Rect(200 + i * 100, upper_pipe_height, 50, 20))
 
   # Game loop
   while True:
@@ -31,10 +36,10 @@ def main():
         pygame.quit()
         sys.exit()
       elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-        bird.y -= 30 
+        bird.y -= 30
 
     # Update the bird
-    bird.y += 5  
+    bird.y += 5
 
     # Check if the bird hits the ground
     if bird.y > 397:
@@ -43,16 +48,16 @@ def main():
 
     # Check if the bird hits a pipe
     collision_rect = pygame.Rect(bird.x, bird.y, bird.width, bird.height)
-    if collision_rect.colliderect(pipe) or collision_rect.colliderect(pipe.topleft) or collision_rect.colliderect(pipe.bottomright):
-      pygame.quit()
-      sys.exit()
-
+    for pipe in pipes:
+      if collision_rect.colliderect(pipe):
+        pygame.quit()
+        sys.exit()
 
     # Update the pipes
     for i in range(0, len(pipes) - 1):
       pipes[i].x -= 5
       pipes[-1] = pipes[0].copy()
-      pipes[0].x = 500
+      pipes[0].x = 600
 
     # Draw the background
     screen.blit(background_image, (0, 0))
@@ -64,10 +69,8 @@ def main():
     for pipe in pipes:
       if pipe.y < 397:
         screen.blit(pipe_image, pipe)
-      else:
-        pipe_height = random.randint(0, 300)
-        pipe = pygame.Rect(500, pipe_height, 100, 200)
-        pipes.append(pipe)
+        # Also blit the upper pipe at the top edge
+        screen.blit(pipe_image, (pipe.x, 0))
 
     # Update the display
     pygame.display.flip()
@@ -77,3 +80,4 @@ def main():
 
 if __name__ == "__main__":
   main()
+ 
